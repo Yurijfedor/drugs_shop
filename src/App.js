51ea693e5fa8146e22855ad9ecb4cdece30ca0c2
Yuriy Shaklak <yurijfedor178@gmail.com>
2024-03-08 23:18:49 +0200

    fix routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShopPage from "./pages/shopPage/ShopPage";
 import ShoppingCartPage from "./pages/shoppingCartPage/ShoppingCartPage";
 import History from "./pages/history/HistoryPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import Header from "./components/common/header/header";
 import Footer from "./components/common/footer/footer";
 
@@ -12,10 +13,11 @@ function App() {
       <div className="app">
         <Header />
         <Routes>
-          <Route path="/" element={<ShopPage />} />
+          <Route index element={<ShopPage />} />
           <Route path="/shop" element={<ShopPage />} />
           <Route path="/shopping-cart" element={<ShoppingCartPage />} />
           <Route path="/history" element={<History />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
         <Footer />
       </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from "react-router-dom";
 import { store, persistor } from "./redux/store";
 import App from "./App";
 
@@ -12,9 +11,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/">
-          <App />
-        </BrowserRouter>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
