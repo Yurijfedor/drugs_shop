f21de947a1221c2bc59424336a803eb3cc7e4880
Yuriy Shaklak <yurijfedor178@gmail.com>
2024-03-08 22:54:55 +0200

    add browserRouter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
 import { store, persistor } from "./redux/store";
 import App from "./App";
 
@@ -11,7 +12,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter basename="/drugs_shop">
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
