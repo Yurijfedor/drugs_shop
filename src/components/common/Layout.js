e48fc01417b4ad516eb0b1f5eb81ae16d0ad3ab3
Yuriy Shaklak <yurijfedor178@gmail.com>
2024-03-09 00:03:31 +0200

    fix routes

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../appBar/header";
 import { Suspense } from "react";
 
 const Layout = () => {
   return (
     <div>
-      <Header />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
